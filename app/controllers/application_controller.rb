3f50b515308f2270dbb0b95b0c9e30814cccd152
robst <robertst81@gmail.com>
2015-10-18 07:44:10 +0200

    moved logic into application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :load_collection, only: :index
+
+  def index; end
+
+  private
+
+  def load_collection
+    @collection = klass.all
+  end
+
+  def klass
+    controller_name.classify.constantize
+  end
+
 end
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,3 @@
 class ArticlesController < ApplicationController
-  before_filter :load_articles, only: :index
 
-  def index;  end
-
-  private
-
-  def load_articles
-    @articles = Article.all
-  end
 end
