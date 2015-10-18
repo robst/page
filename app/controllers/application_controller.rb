698de961d28b6a99a44f281d504cd54307ba6d94
robst <robertst81@gmail.com>
2015-10-18 07:49:35 +0200

    added new action for articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_filter :load_collection, only: :index
+  before_filter :build_new_object, only: [:new, :create]
 
   def index; end
+  def new; end
 
   private
 
   def load_collection
     @collection = klass.all
+  end
+
+  def build_new_object
+    @object = klass.new
   end
 
   def klass
