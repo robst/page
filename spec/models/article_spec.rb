9956b99f06d85b1c09c6409a4ba0b5f7ca568ff1
robst <robertst81@gmail.com>
2015-10-24 20:46:13 +0200

    added specs for article  validations

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryGirl.build :article}
+
+  describe 'validations' do
+    it { is_expected.to be_valid }
+
+    it { is_expected.to validate_presence_of(:title) }
+    it { is_expected.to validate_presence_of(:content) }
+
+    it { is_expected.to validate_uniqueness_of(:title) }
+  end
 end
