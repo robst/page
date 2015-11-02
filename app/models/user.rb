da55e6af465ddff8c085246677381d1062d86ef9
robst <robertst81@gmail.com>
2015-11-02 20:43:42 +0100

    added logic for create new user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,30 @@
 class User < ActiveRecord::Base
+  attr_accessor :password
+  
+  before_save :encrypt_password
+
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true, on: :create, confirmation: true
+
+  def self.authenticate(username, password)
+    user = find_by(username: username)
+    if user && user.password_hash == user.generate_hash(password)
+      user
+    else
+      nil
+    end
+  end
+
+  def generate_hash plain
+    BCrypt::Engine.hash_secret(plain, password_salt)
+  end
+
+private
+
+  def encrypt_password
+    if password.present?
+      self.password_salt = BCrypt::Engine.generate_salt
+      self.password_hash = generate_hash password
+    end
+  end
 end
