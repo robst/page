142ee3e8104bada347bbc12384a3a514dfaccff0
robst <robertst81@gmail.com>
2015-11-02 20:43:08 +0100

    extend specs for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { FactoryGirl.build :user}
+
+  describe 'validations' do
+    it { is_expected.to be_valid }
+
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_presence_of(:password).on(:create) }
+
+    it { is_expected.to validate_uniqueness_of(:username) }
+  end
 end
