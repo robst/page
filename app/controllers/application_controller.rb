badf445a1c4199de5bd69564f7a03a65633e8dc1
robst <robertst81@gmail.com>
2015-11-02 20:45:12 +0100

    added helper method current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  helper_method :current_user
+
   def index
     load_collection
   end
@@ -74,6 +76,10 @@ class ApplicationController < ActionController::Base
 
   def param_class_name
     klass.to_s.underscore.to_sym
+  end
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
 end
