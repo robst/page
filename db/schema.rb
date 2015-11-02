af82f562bf5f648feba4880cb51957957a035771
robst <robertst81@gmail.com>
2015-11-02 20:17:24 +0100

    added authentification fields to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017054153) do
+ActiveRecord::Schema.define(version: 20151102191644) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -25,8 +25,11 @@ ActiveRecord::Schema.define(version: 20151017054153) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+    t.string   "username"
+    t.string   "password"
+    t.string   "password_salt"
   end
 
 end
