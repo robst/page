bfded8fd525e77062df98313d57870ff0a5a8678
robst <robertst81@gmail.com>
2015-11-21 21:30:01 +0100

    fill before_save method for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,12 @@ class ArticlesController < ApplicationController
 
   before_action :authentificate!, except: [:index, :show]
 
+
   private
+
+  def before_save
+    @object.user = current_user
+  end
   
   def object_params
     params.require(param_class_name).permit(:title, :content)
