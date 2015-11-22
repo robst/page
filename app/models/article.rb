bf0e65185dbfee9d301c3c34df0b5663ea658a39
robst <robertst81@gmail.com>
2015-11-22 20:46:37 +0100

    added HABTM for tags and articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   belongs_to :user
 
+  has_and_belongs_to_many :tags
+
   validates :title, presence: true, uniqueness: true
   validates :content, presence: true
   validates :user, presence: true
