62d7b89ed15dce08b9bb717524792058e4370541
robst <robertst81@gmail.com>
2015-11-22 07:25:35 +0100

    moved session helper methods to session helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
 module ApplicationHelper
-  def welcome_message
-    return unless logged_in?
-    I18n.t('welcome', username: current_user.username)
-  end
-
-  def logout_link
-    return unless logged_in?
-    link_to I18n.t('logout'), log_out_path
-  end 
+ 
 end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,11 @@
+module SessionsHelper
+  def welcome_message
+    return unless logged_in?
+    I18n.t('welcome', username: current_user.username)
+  end
+
+  def logout_link
+    return unless logged_in?
+    link_to I18n.t('logout'), log_out_path
+  end
+end
