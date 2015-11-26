2e4b97bb9bbd31fbb54693301284fcd005499767
robst <robertst81@gmail.com>
2015-11-26 19:27:39 +0100

    test to_s method

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,12 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  subject { FactoryGirl.build :tag}
+  subject { FactoryGirl.build :tag, name: 'CrazyTag' }
 
   describe 'validations' do
     it { is_expected.to be_valid }
     it { is_expected.to validate_presence_of(:name) }
 
     it { is_expected.to validate_uniqueness_of(:name) }
+  end
+
+  describe 'methods' do
+    it { expect(subject.to_s).to eq(subject.name)}
   end
 end
