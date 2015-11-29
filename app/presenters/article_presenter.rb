94ef438ce5c007477a9f576b8bacc444b713dd14
robst <robertst81@gmail.com>
2015-11-29 16:31:27 +0100

    added excerpt option for displayed content

--- app/presenters/article_presenter.rb
+++ app/presenters/article_presenter.rb
@@ -1,7 +1,17 @@
 class ArticlePresenter < BasePresenter
 
   def self.present_collection collection, view
-    collection.collect{ |object| ArticlePresenter.new(object, view) }
+    collection.collect{ |object| ArticlePresenter.new(object, view, excerpt: true) }
+  end
+
+  def display_content
+    return stripped_content if options[:excerpt]
+    content
+  end
+
+  def stripped_content
+    return "#{content[0..200]} [...]" if content.length > 200
+    content[0..200]
   end
 
   def tagged
