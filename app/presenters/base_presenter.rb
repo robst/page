f2bce606468920ef30f61035e47ce00febbd1619
robst <robertst81@gmail.com>
2015-11-29 16:31:09 +0100

    added optional options

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,11 +1,16 @@
 class BasePresenter < SimpleDelegator
-
-  def initialize model, view
+  attr_accessor :options
+  def initialize model, view, options={}
     @view = view
+    self.options = available_options.merge options
     super(model)
   end
 
   private
+
+  def available_options
+    {excerpt: false}
+  end
 
   def h
     @view
