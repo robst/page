c05e9542c1da19c5d0a55e5ef7d43e81a3a45554
robst <robertst81@gmail.com>
2015-12-08 20:29:44 +0100

    refactoring of the login process with login class

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,19 +1,41 @@
 class SessionsController < ApplicationController
+  before_action :load_object, only: [:new, :create]
+
   def new
   end
 
   def create
-    user = Authentification.login params
-    if user
-      session[:user_id] = user.id
-      redirect_to root_url
-    else
-      render :new
-    end
+    save_object || render_new
   end
 
   def destroy
     session[:user_id] = nil
     redirect_to root_url
+  end
+
+  private
+
+  def load_object
+    @login = Login.new(login_params)
+  end
+
+  def save_object
+    if @login.save
+      session[:user_id] = @login.user.id
+      render_index
+    end
+  end
+
+  def render_index
+    redirect_to root_url
+  end
+
+  def render_new
+    render :new
+  end
+
+  def login_params
+    return if params[:login].blank?
+    params.require(:login).permit(:username, :password)
   end
 end
